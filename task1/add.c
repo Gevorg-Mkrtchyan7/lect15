d36fff1e8cbf0f9ce9e1bb1090d3cfb0e80b0a22
Gevorg-Mkrtchyan7 <gevorgmkrtchyan744@gmail.com>
2025-04-03 22:12:00 +0400

    tnayin

---
+++ task1/add.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "main.c"
+
+int add(int a, int b){
+	return a + b;
+}
---
+++ task1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int add(int, int);
+
+int main(){
+	int a = 3;
+	int b = 5;
+	return 0;
+}
