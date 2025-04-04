613e17fd9066b316ce79d9aa728278b83bdd4de2
Gevorg-Mkrtchyan7 <gevorgmkrtchyan744@gmail.com>
2025-04-04 13:46:40 +0400

    tnayin

---
+++ task6/cels_fahr.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "main.c"
+
+float cels_fahr(float c){
+	return (c * 9 / 5) + 32;
+}
---
+++ task6/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+float cels_fahr(float);
+
+int main(){
+
+	float c = 5;
+}
