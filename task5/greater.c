8bf7700caa81f6445ad050251b3200943809c3af
Gevorg-Mkrtchyan7 <gevorgmkrtchyan744@gmail.com>
2025-04-04 13:46:33 +0400

    tnayin

---
+++ task5/greater.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.c" 
+
+int greater(int a, int b){
+	if(a > b){
+		return a;
+	}else{
+		return b;
+	}
+}
---
+++ task5/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int pos_neg(int, int);
+
+int main(){
+	int a = 3;
+	int b = 4;
+}
